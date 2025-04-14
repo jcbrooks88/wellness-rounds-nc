90784e114d047a52ef9cabea9e4812e1c411f096
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-04-14 15:55:48 -0400

    deploy commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, "dist"), // stays in client/dist
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       "/graphql": {
@@ -12,4 +17,3 @@ export default defineConfig({
     },
   },
 });
-
